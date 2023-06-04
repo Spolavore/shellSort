0d3688df0cbff0f0485923477afb4de9257ecf5f
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-06-04 16:43:54 -0300

    arrumando bugs

--- desafio.py
+++ desafio.py
@@ -4,7 +4,7 @@
 # de trocas realizada por ele .
 def insertion_sort(str):
     arr = list(str)
-    arr = arr[0:len(arr)-1]
+    arr = arr[0:len(arr)]
     trocas = 0
     for i in range(1, len(arr)):
         key = arr[i]
@@ -17,37 +17,25 @@ def insertion_sort(str):
     return trocas
 
 
-# parte de leitura de arquivo e obtencao da quantidade
-# de datasets e informacoes utilizaveis
-with open('inputs.txt', 'r') as f:
-    lines = f.readlines()
 
-
-qnt_dataset = int(lines[0])
-info = lines[2:]
-
+qnt_dataset = int(input())
 while(qnt_dataset > 0):
-  
-# parte do codigo que pega os dados
-# do arquivo lido e separa em variaveis inicio:
-    dataset_info = info[0].split() 
-    tamanho_strings = int(dataset_info[0])
-    quantidade_strings = int(dataset_info[1])
-    info = info[1:] # remove as informacoes de tamanho_string e quantidade_strings ja lida
-    dataset = info[0: quantidade_strings] # pega o dataset
-    
+    input()
+    # parte do codigo que pega os dados
+    # do arquivo lido e separa em variaveis inicio:
+    dataset_info = input()
+    quantidade_strings = int(dataset_info.split()[1])
+    dataset = []
+    i = 0
+    while(i < quantidade_strings):
+        dataset.append(input())
+        i += 1
     hashMap = {} 
-#inicio da verificacao da desordenacao de cada string do dataset
+    #inicio da verificacao da desordenacao de cada string do dataset
     for i in range(0, quantidade_strings):
         hashMap[f'{dataset[i]}'] = insertion_sort(dataset[i])
-
     sorted_hash_map = sorted(hashMap.items(), key=lambda x:x[1])# funcao para ordenaro hash map
-
     for i in range(0, quantidade_strings):
-        print((sorted_hash_map[i][0][0:tamanho_strings-1])) #print a sequencia de dna sem o \n
-
+        print(sorted_hash_map[i][0]) # print a sequencia de dna sem o \n
     print('')
-# pega o proximo dataset com suas informacoes removendo a blank line
-    info = info[quantidade_strings+1:] 
     qnt_dataset -= 1
-
