0cc2c5e585bfeabaf9f7e2a9f0885d9b8c927c86
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-06-06 18:41:32 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -17,25 +17,27 @@ def insertion_sort(str):
     return trocas
 
 
-
-qnt_dataset = int(input())
-while(qnt_dataset > 0):
-    input()
-    # parte do codigo que pega os dados
-    # do arquivo lido e separa em variaveis inicio:
-    dataset_info = input()
-    quantidade_strings = int(dataset_info.split()[1])
-    dataset = []
-    i = 0
-    while(i < quantidade_strings):
-        dataset.append(input())
-        i += 1
-    hashMap = {} 
-    #inicio da verificacao da desordenacao de cada string do dataset
-    for i in range(0, quantidade_strings):
-        hashMap[f'{dataset[i]}'] = insertion_sort(dataset[i])
-    sorted_hash_map = sorted(hashMap.items(), key=lambda x:x[1])# funcao para ordenaro hash map
-    for i in range(0, quantidade_strings):
-        print(sorted_hash_map[i][0]) # print a sequencia de dna sem o \n
-    print('')
-    qnt_dataset -= 1
+def desafio():
+    qnt_dataset = int(input())
+    while(qnt_dataset > 0):
+        input()
+        # parte do codigo que pega os dados
+        # do arquivo lido e separa em variaveis inicio:
+        dataset_info = input()
+        quantidade_strings = int(dataset_info.split()[1])
+        dataset = []
+        i = 0
+        while(i < quantidade_strings):
+            dataset.append(input())
+            i += 1
+        hashMap = {} 
+        #inicio da verificacao da desordenacao de cada string do dataset
+        for i in range(0, quantidade_strings):
+            hashMap[f'{dataset[i]}'] = insertion_sort(dataset[i])
+        sorted_hash_map = sorted(hashMap.items(), key=lambda x:x[1])# funcao para ordenaro hash map
+        for i in range(0, quantidade_strings):
+            print(sorted_hash_map[i][0]) # print a sequencia de dna sem o \n
+        print('')
+        qnt_dataset -= 1
+    return 0
+desafio()
